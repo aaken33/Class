e1245b82c811a69fd1095eccd1b4de2eeecac49b
Tortolonch <77297919+Tortolonch@users.noreply.github.com>
2024-07-24 23:18:40 -0500

    Update # New VM - ISO Optional.ps1
    og was a older version

--- Scripts/# New VM - ISO Optional.ps1
+++ Scripts/# New VM - ISO Optional.ps1
@@ -1,11 +1,13 @@
 # New VM - ISO Optional 
+#Version #1 2024-07-24
+#-------------------------------------------
 
 # Set CSV file path
-$csvPath = "C:\Class\vmdummyuserdata.csv"
+$csvPath = "C:\Users\Patrick\Downloads\0724vmCSV.csv"
 
 # Check if the file exists
 if (-not (Test-Path $csvPath)) {
-    Write-Host "CSV file not found at '$csvPath'"
+    Write-Host "CSV file not found at '$csvPath. Please make sure the filepath is correct."
     exit
 }
 
@@ -13,25 +15,38 @@ if (-not (Test-Path $csvPath)) {
 $vms = Import-Csv -Path $csvPath
 
 
-# Loop through each row in the CSV and create only the first 5 VMs
+# Loop through each row in the CSV and create the VMs
 $vms | ForEach-Object { #can select which objects by adding parameters in this line after '$vms'
 	$vmName = $_.VMName
+		Write-Host "Creating VM named : $vmName."
 	$memoryStartupBytes = [int]$_.vmMemoryStartupBytes
+		Write-Host "--MemoryStartupBytes: $memoryStartupBytes"
 	$memoryStartupMB = $memoryStartupBytes * 1MB # Convert MB to Bytes
+		Write-Host "--MemoryStartupMB: $memoryStartupMB"
 	if(-not ($vmPath = $_.vmPath)){
-		"This data could not be found in your csv file. Please check if it is missing or mispelled."
+		Write-Host "+-The 'vmPath' could not be found in your csv file. Please check if it is missing or mispelled."
+	}else{
+		Write-Host "--vmPath: $vmPath"
 	}
 	$switchName = $_.vmSwitch
+		Write-Host "--Switch: $switchName"
+
+	$vmGen = $_.vmGeneration
+		Write-Host "--Generation: $vmGen"
+
+	$vhdPath = $_.vhdPath
+		Write-Host "--vhdPath: $vhdPath"
 
 	# Create the VM
-	New-VM -Name $vmName -MemoryStartupBytes $memoryStartupMB -Path $vmPath -SwitchName $switchName
+	New-VM -Name $vmName -MemoryStartupBytes $memoryStartupMB -SwitchName $switchName -Generation $vmGen -vhdPath $_.vhdPath
+		Write-Host "Created VM: $vmName"
 
 	#check if path for VHD is assigned
-	if ($_.vhdPath) {
+	#if ($_.vhdPath) {
 		$vhdPath = $_.vhdPath
-	} else {
-		$vhdPath = "$vmPath\$vmName.vhdx"
-	}
+	#} else {
+		#$vhdPath = "$vmPath\$vmName.vhdx"
+	#}
 
 	Write-Host “vhdPath is set to: $vhdPath”
 
@@ -40,15 +55,16 @@ $vms | ForEach-Object { #can select which objects by adding parameters in this l
 	$vhdSize = $vhdSizeGB * 1MB
 
 	# Create a VHD for the VM
-	New-VHD -Path $vhdPath -SizeBytes $vhdSize -Dynamic
+	#New-VHD -Path $vhdPath -SizeBytes $vhdSize -Dynamic
 
 	# Attach the VHD to the VM
-	Add-VMHardDiskDrive -VMName $vmName -Path $vhdPath
+	#Add-VMHardDiskDrive -VMName $vmName -Path $vhdPath
 
 	# Set DVD Drive with ISO if isoPath is provided 
 	if ($vm.isoPath) {
 		Add-VMDvdDrive -VMName $vmName -Path $Path 
+		Write-Host "with ISO: $isoPath"
 	}
 
-	Write-Host "Created VM: $vmName with ISO: $isoPath"
+	Start-VM -Name $vmName
 }
